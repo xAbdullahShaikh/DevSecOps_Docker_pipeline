14961302a87ef7f05929579cf1c1f77c7283e219
C0D3NaM3AB <123714247+xAbdullahShaikh@users.noreply.github.com>
2025-02-17 13:06:37 +0500

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,11 +1,11 @@
-from src.A1 import add_matrices
+from src.A1 import test_add_matrices
 
 
 # Example Usage
 A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 B = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
 
-result = add_matrices(A, B)
+result = test_add_matrices(A, B)
 
 # Print the result
 for row in result:
