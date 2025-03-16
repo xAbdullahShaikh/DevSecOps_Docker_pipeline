ca645ea9bbd27a1151175b024baad6cb8bb0cf63
C0D3NaM3AB <123714247+xAbdullahShaikh@users.noreply.github.com>
2025-03-16 20:57:41 +0500

    Delete test_code.py

--- test_code.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from src.assignment1 import add_matrices  # Import the function
-
-def test_add_matrices():
-    A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    B = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
-    expected_result = [[10, 10, 10], [10, 10, 10], [10, 10, 10]]
-
-    assert add_matrices(A, B) == expected_result
